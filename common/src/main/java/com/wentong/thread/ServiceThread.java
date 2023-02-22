375272dbc2a258e03a19b1a5ac3691ddce83acc7
zhouwentong <zhouwentong@hualala.com>
2023-02-22 14:15:03 +0800

    add thread support add queue

--- common/src/main/java/com/wentong/thread/ServiceThread.java
+++ common/src/main/java/com/wentong/thread/ServiceThread.java
@@ -1,19 +1,57 @@
 package com.wentong.thread;
 
-public abstract class ServiceThread implements Runnable{
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-    private volatile boolean stopped = false;
+public abstract class ServiceThread implements Runnable {
+
+    private volatile boolean stopped = true;
 
     private Thread thread;
+    protected final CountDownLatch2 waitPoint = new CountDownLatch2(1);
+    protected volatile AtomicBoolean hasNotified = new AtomicBoolean(false);
 
-    abstract String getServiceName();
+    public abstract String getServiceName();
 
     // 用父类的 start 修饰子类的 start 方法
     public void start() {
+        stopped = false;
         thread = new Thread(this, getServiceName());
         thread.setDaemon(true);
         thread.start();
     }
 
+    public boolean isStopped() {
+        return stopped;
+    }
+
+    public void shutdown() {
+        stopped = true;
+        thread.interrupt();
+    }
+
+    // 等待多久，再次执行。
+    // 这里利用了重写的 CountDownLatch2 的 reset 功能
+    protected void waitForRunning(long interval) {
+
+        if (hasNotified.compareAndSet(true, false)) {
+            onWaitEnd();
+            return;
+        }
+
+        waitPoint.reset();
+        try {
+            waitPoint.await(interval, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            hasNotified.set(false);
+            waitPoint.countDown();
+            onWaitEnd();
+        }
+    }
+
+    protected void onWaitEnd() {
+    }
 
 }
--- core/src/main/java/com/wentong/queue/UrlSaverConsumer.java
+++ core/src/main/java/com/wentong/queue/UrlSaverConsumer.java
@@ -1,26 +1,35 @@
 package com.wentong.queue;
 
+import com.google.common.collect.Lists;
+import com.wentong.thread.ServiceThread;
+import com.wentong.vo.TinyUrl;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 /**
  * 对队列进行消费，进行批量插入操作
  */
-public class UrlSaverConsumer {
+public class UrlSaverConsumer extends ServiceThread {
 
-    private static volatile boolean start = false;
+    private static final int BATCH_SIZE = 1000;
+    List<TinyUrl> list = Lists.newArrayListWithCapacity(BATCH_SIZE);
+    private static CountDownLatch latch = new CountDownLatch(1);
 
-    public static synchronized void start() {
-        start = true;
+    @Override
+    public void run() {
+        while (!isStopped()) {
+            try {
+                latch.await(1, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
-    public static synchronized void stop() {
-        start = false;
+    @Override
+    public String getServiceName() {
+        return "urlSaverConsumer";
     }
-
-    public static synchronized boolean isStart() {
-        return start;
-    }
-
-
-
-
-
 }
